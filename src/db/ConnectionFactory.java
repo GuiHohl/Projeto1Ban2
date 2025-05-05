50d53fc98ca6a29313b6476fb5732b7b65c75f71
Guilherme <gwhohl06@gmail.com>
2025-05-05 01:19:11 -0300

    ajustes

--- src/db/ConnectionFactory.java
+++ src/db/ConnectionFactory.java
@@ -6,13 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-//    private static final String URL = "jdbc:postgresql://pg-232ae497-projeto-1-ban-2.k.aivencloud.com:26812/defaultdb";
-//    private static final String USER = "avnadmin";
-//    private static final String PASSWORD = "AVNS_-RBKoyNX1rJeVpMdxI2";
-
-    private static final String URL = "jdbc:postgresql://localhost:5432/ProjetoBan2";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "12345";
+    private static final String URL = "jdbc:postgresql://pg-232ae497-projeto-1-ban-2.k.aivencloud.com:26812/defaultdb";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_-RBKoyNX1rJeVpMdxI2";
 
     public static Connection getConnection() {
         try {
